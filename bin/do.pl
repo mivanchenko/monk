70531774d1f4fd37deb569e6e999a93890827243
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2014-12-23 21:57:29 +0200

    controller, templates added, link activated

--- bin/do.pl
+++ bin/do.pl
@@ -6,8 +6,26 @@ use warnings;
 use CGI;
 
 my $q = CGI->new;
-my @keywords = $q->keywords;
+my ($keyword) = $q->keywords;
 
-print "Content-Type: text/plain\n\n";
-print "do.pl\n";
-print "@keywords\n";
+my $map = {
+	'6' => 'Молитви/Шостопсалміє',
+};
+
+exit unless exists $map->{$keyword};
+
+my $header = do '../tpl/header';
+my $body   = '';
+my $footer = do '../tpl/footer';
+
+my $path = "../src/$map->{$keyword}";
+open my $fh, '<', $path
+	or die "Can't open $path: [$!]";
+while ( my $line = <$fh> ) {
+	chomp $line;
+	$body .= "<p>$line</p>";
+}
+close $fh;
+
+print "Content-Type: text/html; charset=utf-8\n\n";
+print $header.$body.$footer;
