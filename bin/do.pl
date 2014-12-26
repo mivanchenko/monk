d3b0f236147ebaa0d94f90fa5b3f7a8ae592d891
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2014-12-26 11:39:50 +0200

    Angular added, src converted to Markdown, Angular converts Markdown to HTML

--- bin/do.pl
+++ bin/do.pl
@@ -9,23 +9,14 @@ my $q = CGI->new;
 my ($keyword) = $q->keywords;
 
 my $map = {
-	'6' => 'Молитви/Шостопсалміє',
+	'6'    => 'Молитви/Шостопсалміє',
 };
 
 exit unless exists $map->{$keyword};
 
-my $header = do '../tpl/header';
-my $body   = '';
-my $footer = do '../tpl/footer';
-
-my $path = "../src/$map->{$keyword}";
-open my $fh, '<', $path
-	or die "Can't open $path: [$!]";
-while ( my $line = <$fh> ) {
-	chomp $line;
-	$body .= "<p>$line</p>";
-}
-close $fh;
+my $header = do { local( @ARGV, $/ ) = '../tpl/header'; <> };
+my $body   = do { local( @ARGV, $/ ) = "../src/$map->{$keyword}"; <> };
+my $footer = do { local( @ARGV, $/ ) = '../tpl/footer'; <> };
 
 print "Content-Type: text/html; charset=utf-8\n\n";
 print $header.$body.$footer;
