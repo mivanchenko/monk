46f2c0524e1c114702fd39dd53ec719283f2686d
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2015-01-02 15:27:17 +0200

    Prayer added, headers reorganized (pictures are allowed to have links), pictures added

--- bin/do.pl
+++ bin/do.pl
@@ -6,15 +6,19 @@ use warnings;
 my $keyword = $ENV{'QUERY_STRING'};
 $keyword =~ s/\W//g;
 
-# 'pic' is optional
 my $map = {
-	'6'      => {
+	'6' => {
 		'src' => 'Молитви/Шостопсалміє',
 		'pic' => 'vor-monah_25_90.jpg',
 	},
 	'letter' => {
 		'src' => 'Молитви/Лист',
+		'pic' => 'sts3-12_15_90.jpg',
 	},
+	'narod' => {
+		'src' => 'Молитви/За народ',
+		'pic' => '125502_20_90.jpg',
+	}
 };
 
 if ( ! exists $map->{$keyword} ) {
@@ -22,11 +26,22 @@ if ( ! exists $map->{$keyword} ) {
 	exit;
 }
 
-my $header = do { local( @ARGV, $/ ) = '../tpl/header'; <> };
-my $body   = do { local( @ARGV, $/ ) = "../src/$map->{$keyword}{'src'}"; <> };
-my $footer = do { local( @ARGV, $/ ) = '../tpl/footer'; <> };
+my $header1 = do { local( @ARGV, $/ ) = '../tpl/header1'; <> };
+my $header2 = '';
 
-$header =~ s/%(\w+)%/$map->{$keyword}{$1}/g;
+if ( -r "../tpl/header2/$keyword" ) {
+	$header2 = do { local( @ARGV, $/ ) = "../tpl/header2/$keyword"; <> };
+}
+else {
+	$header2 = do { local( @ARGV, $/ ) = "../tpl/header2/default"; <> };
+}
+
+my $header3 = do { local( @ARGV, $/ ) = '../tpl/header3'; <> };
+my $body    = do { local( @ARGV, $/ ) = "../src/$map->{$keyword}{'src'}"; <> };
+my $footer  = do { local( @ARGV, $/ ) = '../tpl/footer'; <> };
+
+my $header = join( "\n", $header1, $header2, $header3 );
+$header =~ s/%(\w+)%/$map->{$keyword}{$1}/sg;
 
 print "Content-Type: text/html; charset=utf-8\n\n";
 print $header.$body.$footer;
