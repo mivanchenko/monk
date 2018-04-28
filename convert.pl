78576e27716dd6401de155d8c512807917c720fd
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2018-04-28 17:20:07 +0300

    Add slash to all css

--- convert.pl
+++ convert.pl
@@ -6,76 +6,74 @@ use warnings;
 use Perl6::Slurp;
 use Text::MultiMarkdown 'markdown';
 
-#my $pages = {
-#	6 => {},
-#	anthem => {
-#		face => 'herb_15_90.png',
-#	},
-#	christen => {},
-#	evening => {},
-#	'from-bible' => {},
-#	'from-v' => {},
-#	'from-kuksha' => {},
-#	happy => {},
-#	letter => {
-#		head => '<div align="center"><a href="http://molytva.at.ua/index/khresna_doroga/0-65"><img src="../img/sts3-12_15_90.jpg" /></a></div>'
-#	},
-#	morning => {},
-#	nation => {
-#		face => '125502_20_90.jpg',
-#	},
-#	protest => {},
-#	serb => {},
-#	thank => {},
-#	'ukr-sins' => {},
-#	wife => {
-#		face => 'christianmarriage-200x138.jpg',
-#	},
-#};
-#
-#my $head_begin   = '<div align="center"><img src="../img/';
-#my $face_default = 'sonce.jpg';
-#my $head_end     = '" /></div>' . "\n\n";
-#
+my $pages = {
+	6 => {},
+	anthem => {
+		face => 'herb_15_90.png',
+	},
+	christen => {},
+	evening => {},
+	'from-bible' => {},
+	'from-v' => {},
+	'from-kuksha' => {},
+	happy => {},
+	letter => {
+		head => '<div align="center"><a href="http://molytva.at.ua/index/khresna_doroga/0-65"><img src="../img/sts3-12_15_90.jpg" /></a></div>'
+	},
+	morning => {},
+	nation => {
+		face => '125502_20_90.jpg',
+	},
+	protest => {},
+	serb => {},
+	thank => {},
+	'ukr-sins' => {},
+	wife => {
+		face => 'christianmarriage-200x138.jpg',
+	},
+};
+
+my $head_begin   = '<div align="center"><img src="../img/';
+my $face_default = 'sonce.jpg';
+my $head_end     = '" /></div>' . "\n\n";
+
 my $header = slurp 'tpl/header';
 my $footer = slurp 'tpl/footer';
-#
-#foreach my $keyword ( sort keys %{$pages} ) {
-#	my $page = $pages->{$keyword};
-#
-#	my $head;
-#
-#	if ( $page->{head} ) {
-#		$head = $page->{head};
-#	}
-#	elsif ( $page->{face} ) {
-#		$head = $head_begin . $page->{face} . $head_end;
-#	}
-#	else {
-#		$head = $head_begin . $face_default . $head_end;
-#	}
-#
-#	my $foot = $page->{foot} || '';
-#
-#	my $md;
-#	eval { $md = slurp "src/$keyword" };
-#	if ( $@ ) {
-#		$md = slurp "src/$keyword.md"
-#	}
-#	my $body = markdown $md;
-#
-#	my $dest = $keyword;
-#	open my $dest_fh, '>', $dest
-#		or die "Can't open $dest for writing: $!";
-#	print $dest_fh join( "\n", $header, $head, $body, $foot, $footer );
-#	close $dest_fh;
-#}
+
+foreach my $keyword ( sort keys %{$pages} ) {
+	my $page = $pages->{$keyword};
+
+	my $head;
+
+	if ( $page->{head} ) {
+		$head = $page->{head};
+	}
+	elsif ( $page->{face} ) {
+		$head = $head_begin . $page->{face} . $head_end;
+	}
+	else {
+		$head = $head_begin . $face_default . $head_end;
+	}
+
+	my $foot = $page->{foot} || '';
+
+	my $md;
+	eval { $md = slurp "src/$keyword" };
+	if ( $@ ) {
+		$md = slurp "src/$keyword.md"
+	}
+	my $body = markdown $md;
+
+	my $dest = $keyword;
+	open my $dest_fh, '>', $dest
+		or die "Can't open $dest for writing: $!";
+	print $dest_fh join( "\n", $header, $head, $body, $foot, $footer );
+	close $dest_fh;
+}
 
 # convert pestov
 
-#use Data::Printer;
-my @sources = qx{ find src/pestov | grep md\$ };
-#p @sources;
+my @sources = qx{ find src/pestov | grep \.md\$ };
 
 foreach my $source ( @sources ) {
 	chomp $source;
@@ -85,7 +83,6 @@ foreach my $source ( @sources ) {
 	$dest =~ s{^src/}{};
 	$dest =~ s{index\.md$}{index.html};
 	$dest =~ s{\.md$}{};
-#	p $dest;
 	open my $dest_fh, '>', $dest
 		or die "Can't open $dest for writing: $!";
 	print $dest_fh join("\n", $header, $html_body, $footer);
